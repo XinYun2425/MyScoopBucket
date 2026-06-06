57a74e4f125b2f553f952ee746b061f09686ca58
昕昀です <xinyun233@qq.com>
2026-06-06 10:08:17 +0800

    Update upstream parameter in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "xinyun2425/MyScoopBucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
